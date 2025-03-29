76bb3a5d434ebb0e7d4b9b03f20162be262dae2c
Panczel24 <pcsenge8@gmail.com>
2025-03-29 13:41:34 +0100

    BankCard

---
+++ BankUjra/BankCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankUjra
+{
+    internal class BankCard : BankingService
+    {
+        double szamla;
+        int kartyaSzam;
+        public BankCard(Owner owner, double szamla, int kartyaSzam) : base(owner)
+        {
+            this.szamla = szamla;
+            this.kartyaSzam = kartyaSzam;
+        }
+
+        public double Szamla { get => szamla;}
+        public int KartyaSzam { get => kartyaSzam; }
+
+        public bool Purchase(double osszeg)
+        {
+            if (szamla - osszeg >= 0)
+            {
+                szamla -= osszeg;
+                return true;
+            }
+            return false;
+        }
+    }
+}
